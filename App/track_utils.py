e27013e242cdd33c32d0fa9099748e1ba29f9c3d
Sarvesh Kulkarni <sarvesh123305@gmail.com>
2024-04-05 09:26:05 +0530

    Code restructuring

--- App/track_utils.py
+++ App/track_utils.py
@@ -1,31 +1,32 @@
-#Database connection
+# Load Database Pkg
 import sqlite3
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
 
+# Fxn
 def create_page_visited_table():
-	c.execute('create table if not exists pageTrackTable(pagename text,timeOfvisit timestamp)')
+	c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT,timeOfvisit TIMESTAMP)')
 
 def add_page_visited_details(pagename,timeOfvisit):
-	c.execute('insert into pageTrackTable(pagename,timeOfvisit) values(?,?)',(pagename,timeOfvisit))
+	c.execute('INSERT INTO pageTrackTable(pagename,timeOfvisit) VALUES(?,?)',(pagename,timeOfvisit))
 	conn.commit()
 
 def view_all_page_visited_details():
-	c.execute('select * from pageTrackTable')
+	c.execute('SELECT * FROM pageTrackTable')
 	data = c.fetchall()
 	return data
 
 
-# To Track Input & Prediction
+# Fxn To Track Input & Prediction
 def create_emotionclf_table():
-	c.execute('create table if not exists emotionclfTable(rawtext text,prediction text,probability number,timeOfvisit timestamp)')
+	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT,prediction TEXT,probability NUMBER,timeOfvisit TIMESTAMP)')
 
 def add_prediction_details(rawtext,prediction,probability,timeOfvisit):
-	c.execute('insert into emotionclfTable(rawtext,prediction,probability,timeOfvisit) values(?,?,?,?)',(rawtext,prediction,probability,timeOfvisit))
+	c.execute('INSERT INTO emotionclfTable(rawtext,prediction,probability,timeOfvisit) VALUES(?,?,?,?)',(rawtext,prediction,probability,timeOfvisit))
 	conn.commit()
 
 def view_all_prediction_details():
-	c.execute('select * from emotionclfTable')
+	c.execute('SELECT * FROM emotionclfTable')
 	data = c.fetchall()
 	return data
